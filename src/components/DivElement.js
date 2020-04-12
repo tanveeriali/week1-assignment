f356cd525b2f824aff6fdea0c5a128864b37e5dc
Tanveer Ali <tanveer.i.ali@gmail.com>
2020-04-12 12:26:15 -0700

    Add files via upload

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,17 @@
 // Import HTMLElement here
+import HTMLElement from './HTMLElement';
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super('div', content);
+    this.content = content;
+  }
+}
 
 // Export class here
+export default DivElement;
+
+const andIThinkToMyself = new DivElement('What a wonderful world');
+
+console.log(andIThinkToMyself.render());
