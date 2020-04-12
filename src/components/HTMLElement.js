dbdb08dde9c575954efbb4b9a9294ac5c7d31b12
Tanveer Ali <tanveer.i.ali@gmail.com>
2020-04-12 12:25:27 -0700

    Add files via upload
    -finished homework

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,18 @@
 // Define class here
+class HTMLElement {
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
+
+  render() {
+    return `<${this.tag}>${this.content}</${this.tag}>`;
+  }
+}
+
 
 // Export class here
-export default {};
+export default HTMLElement;
+
+const lovelaceQuote = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
+console.log(lovelaceQuote.render());
